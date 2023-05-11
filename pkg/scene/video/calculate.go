07027c4c5e0be26764ac7ddd7b445cf35a75c577
txu <tao.xu2008@outlook.com>
2023-05-11 20:42:29 +0800

    video

--- pkg/scene/video/calculate.go
+++ pkg/scene/video/calculate.go
@@ -7,7 +7,33 @@ func (v *VideoInfo) printVideoInfo() bool {
 }
 
 // 视频监控场景 - 数据模型计算
-func (v *VideoInfo) CalcData() bool {
-	// 每天一路视频吸引写入的数据量
-	return true
+func (v *VideoInfo) CalcData() *VideoInfo {
+	v.SafeWaterCapacity = int(float32(v.TotalCapacity) * v.SafeWaterLevel)
+	if v.PrepareChannelNum < v.ChannelNum {
+		v.PrepareChannelNum = v.ChannelNum
+	}
+
+	// 总带宽=码流/8*路数
+	v.BandWidth = (v.BitStream / 8) * v.ChannelNum
+
+	// 对象数 = 安全水位容量 / 对象大小
+	v.ObjNum = v.SafeWaterCapacity / v.ObjSize
+
+	// 每天数据量 = 带宽 * 1天
+	var sizePD = v.BandWidth * 60 * 60 * 24
+	if v.DataLife == 0 {
+		v.DataLife = float32(v.SafeWaterCapacity / sizePD)
+	}
+
+	// 每天一路视频需要写入的数据量
+	var sizePCPD = (v.BitStream / 8) * 60 * 60 * 24
+
+	// 更加码流+容量+保留期限，换算 支持的视频路数
+	if v.ChannelNum == 0 && v.DataLife > 0 {
+		v.ChannelNum = int(float32(v.SafeWaterCapacity)/v.DataLife) / sizePCPD
+	}
+
+	// 打印计算结果
+	v.printVideoInfo()
+	return v
 }
--- pkg/scene/video/models.go
+++ pkg/scene/video/models.go
@@ -8,9 +8,8 @@ type VideoBaseInfo struct {
 	DataLife   float32 // 数据保留期限，单位 天
 	ObjSize    int     // 对象大小，单位 MiB
 
-	TotalCapacity     int64   // 存储池总容量大小，单位 MiB
-	SafeWaterLevel    float32 // 安全水位，即数据写入存储池的数据量最大不超过总容量的百分比，例如 90%=0.9
-	SafeWaterCapacity int     // 存储池安全水位容量大小，单位 MiB
+	TotalCapacity  int     // 存储池总容量大小，单位 MiB
+	SafeWaterLevel float32 // 安全水位，即数据写入存储池的数据量最大不超过总容量的百分比，例如 90%=0.9
 
 	Appendable   bool // 追加写模式
 	Segments     int  // 追加写模式下，一个对象追加分片次数
@@ -23,14 +22,14 @@ type VideoDataInfo struct {
 	VideoBaseInfo
 	SafeWaterCapacity int // 存储池安全水位容量大小，单位 MiB
 
-	BandWidth    int   // 带宽， MiB/s
-	BucketNum    int   // 桶数量
-	ObjNum       int64 // 安全水位能写入的总对象数量，达到该数量后需要 边写边删
-	ObjNumPC     int64 // 安全水位每路视频能写入的对象数量
-	ObjNumPD     int64 // 每天需要写入的对象数量，码流+对象大小 计算得出
-	ObjNumPCPD   int64 // 每路视频、每天，需要写入的对象数量
-	TimeInterval int   // 一路视频中，没个视频对象产生的实际间隔，单位：秒
-	MaxWorkers   int16 // 一路视频中，最大处理并行数
+	BandWidth    int // 带宽， MiB/s
+	BucketNum    int // 桶数量
+	ObjNum       int // 安全水位能写入的总对象数量，达到该数量后需要 边写边删
+	ObjNumPC     int // 安全水位每路视频能写入的对象数量
+	ObjNumPD     int // 每天需要写入的对象数量，码流+对象大小 计算得出
+	ObjNumPCPD   int // 每路视频、每天，需要写入的对象数量
+	TimeInterval int // 一路视频中，没个视频对象产生的实际间隔，单位：秒
+	MaxWorkers   int // 一路视频中，最大处理并行数
 
 	PrepareConcurrent float32 // 数据预埋阶段，平均每秒处理对象数
 	MainConcurrent    float32 // 写删阶段，平均每秒处理对象数
@@ -41,8 +40,8 @@ type VideoCustomizeInfo struct {
 	PrepareChannelNum int    // 数据预埋阶段，指定视频路数模拟写入（为加快数据预埋），0=ChannelNum即和写删阶段相同
 	BucketPrefix      string // 桶名 前缀
 	ObjPrefix         int    // 对象名 前缀
-	ObjIdxWidth       int64  // 对象名称序号长度， 3=>001
-	ObjIdxStart       int64  // 对象处理 序号起始值
+	ObjIdxWidth       int    // 对象名称序号长度， 3=>001
+	ObjIdxStart       int    // 对象处理 序号起始值
 }
 
 // VideoInfo 视频监控场景 数据模型：原始需求信息 + 数据模型信息 + 自定义变量信息
